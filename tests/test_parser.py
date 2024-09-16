4927abd196f1cb08644f4092c8abc455ed61be38
ChristinaMariaMayrBirg <mayr9@hm.edu>
2024-09-16 16:26:55 +0200

    enable testing

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,17 +1,31 @@
 import os.path
+from logging import Logger
 
+import pytest
 from nomad.client import normalize_all, parse
+from nomad.datamodel import EntryArchive
 
 
-def test_parser():
+from nomad_pedestrian_dynamics_extension.nomadparserexampleMY import ExampleParserChristina
+
+
+def test_parser_2():
+    archive = EntryArchive()
+    parser = ExampleParserChristina()
+    logger = Logger("test")
+
     test_file = os.path.join(
         os.path.dirname(__file__), 'data', 'test.example-format.txt'
     )
-    entry_archive = parse(test_file)[0]
-    normalize_all(entry_archive)
+    parser.parse(test_file, archive, logger)
 
-    assert entry_archive.data.pattern.tolist() == [
+    normalize_all(archive)
+
+    assert archive.data.pattern.tolist() == [
         [1.0, 0.0, 1.0],
         [1.0, 1.0, 1.0],
         [1.0, 0.0, 1.0],
     ]
+
+    print("done")
+
