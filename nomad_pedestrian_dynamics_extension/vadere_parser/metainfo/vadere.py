a95ae2312e918e722308a2b410df56346d45963f
ChristinaMariaMayrBirg <mayr9@hm.edu>
2024-09-20 14:13:01 +0200

    add plot functionalities

--- nomad_pedestrian_dynamics_extension/vadere_parser/metainfo/vadere.py
+++ nomad_pedestrian_dynamics_extension/vadere_parser/metainfo/vadere.py
@@ -1,10 +1,14 @@
 
 import numpy as np
-from nomad.datamodel import ArchiveSection
+from nomad.datamodel import ArchiveSection, EntryData
+from nomad.datamodel.metainfo.plot import PlotlyFigure, PlotSection
 from nomad.datamodel.metainfo.workflow import Workflow
 from nomad.datamodel.results import Properties
 from nomad.datamodel.results import Results
 from nomad.metainfo import Datetime, Package, Quantity,  Section, SubSection, MSection
+import plotly.express as px
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 
 m_package = Package(name='vadere_nomadmetainfo_json', description='None')
 
@@ -116,13 +120,51 @@ class MicroscopicResults(ArchiveSection):
         default = 1.234
     )
 
+
+
+
+
+
+class CustomSection(PlotSection, EntryData):
+    m_def = Section()
+    time = Quantity(type=float, shape=['*'], unit='s', a_eln=dict(component='NumberEditQuantity'))
+    substrate_temperature = Quantity(type=float, shape=['*'], unit='K', a_eln=dict(component='NumberEditQuantity'))
+    chamber_pressure = Quantity(type=float, shape=['*'], unit='Pa', a_eln=dict(component='NumberEditQuantity'))
+
+    def normalize(self, archive, logger):
+        super(CustomSection, self).normalize(archive, logger)
+
+        first_line = px.scatter(x=self.time, y=self.substrate_temperature)
+        second_line = px.scatter(x=self.time, y=self.chamber_pressure)
+        figure1 = make_subplots(rows=1, cols=2, shared_yaxes=True)
+        figure1.add_trace(first_line.data[0], row=1, col=1)
+        figure1.add_trace(second_line.data[0], row=1, col=2)
+        figure1.update_layout(height=400, width=716, title_text="Creating Subplots in Plotly")
+        self.figures.append(PlotlyFigure(label='figure 1', figure=figure1.to_plotly_json()))
+
+        figure2 = px.scatter(x=self.substrate_temperature, y=self.chamber_pressure, color=self.chamber_pressure, title="Chamber as a function of Temperature")
+        self.figures.append(PlotlyFigure(label='figure 2', index=1, figure=figure2.to_plotly_json()))
+
+        heatmap_data = [[None, None, None, 12, 13, 14, 15, 16],
+             [None, 1, None, 11, None, None, None, 17],
+             [None, 2, 6, 7, None, None, None, 18],
+             [None, 3, None, 8, None, None, None, 19],
+             [5, 4, 10, 9, None, None, None, 20],
+             [None, None, None, 27, None, None, None, 21],
+             [None, None, None, 26, 25, 24, 23, 22]]
+
+        heatmap = go.Heatmap(z=heatmap_data, showscale=False, connectgaps=True, zsmooth='best')
+        figure3 = go.Figure(data=heatmap)
+        figure_json = figure3.to_plotly_json()
+        figure_json['config'] = {'staticPlot': True}
+        self.figures.append(PlotlyFigure(label='figure 3', index=0, figure=figure_json))
+
+
 class MacroscopicResults(ArchiveSection):
     m_def = Section()
 
-    densities = Quantity(
-            type=np.float64,
-            shape=['1...*', 3],
-            description="""DUMMY.""",
+    densities = SubSection(
+            sub_section=CustomSection
     )
 
     testdata2= Quantity(
@@ -141,6 +183,10 @@ class VadereProperties(Properties):
 
     #def normalize(self, archive, logger):
       #  pass
+
+
+
+
 
 
 
