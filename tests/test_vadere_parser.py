04f799a8cb37250e4f2d51b5513f02d36b974eb4
ChristinaMariaMayrBirg <mayr9@hm.edu>
2024-09-23 16:33:53 +0200

    fix broken test

--- tests/test_vadere_parser.py
+++ tests/test_vadere_parser.py
@@ -5,20 +5,23 @@ from nomad.client import parse
 from nomad.datamodel import EntryArchive
 from nomad_pedestrian_dynamics_extension.vadere_parser import *
 
+
+data_path = os.path.join(os.path.dirname(__file__), "data", "basic_2_density_discrete_ca_2024-09-23_15-52-24.887",
+                         "basic_2_density_discrete_ca.scenario")
+
+
 def test_vadere_parser_without_matching():
     """
     Tests the parsing algorithm without matching.
     """
     archive = EntryArchive()
-    data_path = os.path.join(os.path.dirname(__file__), "data", "basic_2_density_discrete_ca_2024-08-05_12-33-49.69" , "postvis.traj")
 
     # choose the parser manually
     parser = VadereParser()
     parser.parse(data_path, archive, logger = Logger("Matching_test"))
 
     sim = archive.data
-    assert len(sim.model) == 4 #TODO: needs to be update after finalizing the structure
-    assert len(sim.output) == 1
+    assert len(sim.model) == 3
 
     archive.m_to_dict()
 
@@ -29,7 +32,6 @@ def test_entry_point_configuration():
     This requires that the entry point is correctly specified in pyproject.toml and nomad_pedestrian_dynamics_extension/__init__
     """
 
-    data_path = os.path.join(os.path.dirname(__file__), "data", "basic_2_density_discrete_ca_2024-09-23_15-52-24.887" , "basic_2_density_discrete_ca.scenario")
     archive = parse(data_path)
 
     from nomad.client import normalize_all
