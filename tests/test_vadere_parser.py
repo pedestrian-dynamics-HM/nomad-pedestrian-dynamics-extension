e861140d9965520250c7ff45026d0b0b334f8698
ChristinaMariaMayrBirg <mayr9@hm.edu>
2024-09-23 16:20:59 +0200

    update test results

--- tests/test_vadere_parser.py
+++ tests/test_vadere_parser.py
@@ -30,7 +30,7 @@ def test_entry_point_configuration():
     This requires that the entry point is correctly specified in pyproject.toml and nomad_pedestrian_dynamics_extension/__init__
     """
 
-    data_path = os.path.join(os.path.dirname(__file__), "data", "basic_2_density_discrete_ca_2024-08-05_12-33-49.69" , "basic_2_density_discrete_ca.scenario")
+    data_path = os.path.join(os.path.dirname(__file__), "data", "basic_2_density_discrete_ca_2024-09-23_15-52-24.887" , "basic_2_density_discrete_ca.scenario")
     archive = parse(data_path)
 
     from nomad.client import normalize_all
